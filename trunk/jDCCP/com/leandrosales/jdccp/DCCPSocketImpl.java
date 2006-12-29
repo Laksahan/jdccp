f2925d2685f7b2535270e7619f1a2a41df68814d
leandroal <leandroal@5543e436-7325-0410-beb9-0bc31c706920>
2006-12-29 22:45:05 +0000

    Initial import.

---
+++ trunk/jDCCP/com/leandrosales/jdccp/DCCPSocketImpl.java
@@ -0,0 +1,103 @@
+package com.leandrosales.jdccp;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.SocketAddress;
+import java.net.SocketException;
+import java.net.SocketImpl;
+
+
+public class DCCPSocketImpl extends SocketImpl {
+	
+	private InputStream inputStream;
+	private OutputStream outputStream;
+	
+	public DCCPSocketImpl() {
+		this.inputStream  = new DCCPInputStream();
+		this.outputStream = new DCCPOutputStream();
+	}
+
+	@Override
+	protected void accept(SocketImpl s) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected int available() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected void bind(InetAddress host, int port) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void close() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void connect(String host, int port) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void connect(InetAddress address, int port) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void connect(SocketAddress address, int timeout) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void create(boolean stream) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected InputStream getInputStream() throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected OutputStream getOutputStream() throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void listen(int backlog) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void sendUrgentData(int data) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object getOption(int optID) throws SocketException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setOption(int optID, Object value) throws SocketException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ trunk/jDCCP/com/leandrosales/jdccp/DCCPSocketImplFactory.java
@@ -0,0 +1,12 @@
+package com.leandrosales.jdccp;
+import java.net.SocketImpl;
+import java.net.SocketImplFactory;
+
+
+public class DCCPSocketImplFactory implements SocketImplFactory {
+
+	public SocketImpl createSocketImpl() {
+		return new DCCPSocketImpl();
+	}
+
+}
